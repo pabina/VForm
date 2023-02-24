15bb891e1cdeb4731f49166841ae16cc8c2f7c00
pabina <rimalpabina1234@gmail.com>
2023-02-24 16:53:10 +0545

    react router and pages aded

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MyForm from './pages/Form';
+import View from './pages/view';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+
+const App = () => {
+
+  
   return (
-    <div className="App">
-      
-    </div>
-  );
+<>
+
+
+   
+    <Router>  
+<Routes>
+<Route path="/" element={<MyForm />} />
+<Route path="/view" element={<View/>} />
+
+</Routes>
+    </Router>
+    </>
+   
+  )
 }
 
-export default App;
+export default App
